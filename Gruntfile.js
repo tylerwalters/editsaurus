8d7626ee0bf11c1ef8b6c0096c365e6412045587
Tyler Walters <tyler@tylerwalters.com>
2014-05-28 14:15:05 -0700

    Configuring Gruntfile.js and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,5 +11,48 @@ module.exports = function(grunt) {
 				dest: 'dist/js/script.js'
 			}
 		},
+		sass: {
+			dist: {
+				files: {
+					'app/css/style.css': 'app/css/style.scss'
+				}
+			}
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+			css: {
+				files: {
+					'dist/css/style.min.css': ['<%= concat.css.dest %>']
+				}
+			},
+			js: {
+				files: {
+					'dist/js/script.min.js': ['<%= concat.js.dest %>']
+				}
+			}
+		}, 
+		jshint: {
+			files: ['Gruntfile.js', 'app/js/*.js']
+		},
+		watch: {
+			css: {
+				files: ['<%= sass.files %>'],
+				tasks: ['sass']
+			},
+			js: {
+				files: ['<%= jshint.files %>'],
+				tasks: ['jshint']
+			}
+		}
 	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	grunt.registerTask('deploy', ['jshint', 'sass', 'concat', 'uglify']);
 };
