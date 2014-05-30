73dcade2428b0bc278ff0806c6779c81b8fc8408
Tyler Walters <tyler@tylerwalters.com>
2014-05-30 10:10:39 -0700

    Creating the basic logic behind the text conversion

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+var textInput = '',
+		checkedText = '';
+
+$('#writing-check-button').click(function(e) {
+	e.preventDefault();
+	textInput = $('#writing-check-input').val();
+	checkedText = checkText(textInput);
+	$('#writing-check-output').html(checkedText);
+});
+
+function checkText(text) {
+	console.log('text: ' + text);
+	text = text.replace(/\w*ly\b/g, '<span class="adverb">$&</span>');
+	console.log('text: ' + text);
+	return text;
+}
