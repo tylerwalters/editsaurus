600f30af1c51b16beb5a003fd588e2fadd0857af
Tyler Walters <tyler@tylerwalters.com>
2014-06-16 09:54:16 -0700

    Configuring each check option with the correct regular expression, adding a definitions section to describe what is checked and why

--- app/js/script.js
+++ app/js/script.js
@@ -18,7 +18,7 @@
 	});
 
 	function checkText(text, choices) {
-		text = text.replace(/\r?\n/g, '<br />');
+		text = '<p>' + text.replace(/\r\n|\n\r|\n\n|\r\r/g, '</p><p>') + '</p>';
 		for (var i = 0, max = choices.length; i < max; i++) {
 			text = text.replace(checkOptions[choices[i]].regex, '<span class="' + checkOptions[choices[i]].name + '" title="' + checkOptions[choices[i]].title + '">$&</span>');
 		}
@@ -27,11 +27,11 @@
 
 	var checkOptions = {
 		adverb: {name: 'adverb', regex: /\w*ly\b/g, title: 'This may be an unnecessary adverb.'},
-		fillerWords: {name: 'filler-words', regex: /\w*ly\b/g, title: 'This may be an unnecessary filler word.'},
-		passiveVoice: {name: 'passive-voice', regex: /\w*ly\b/g, title: 'This may be an unnecessary use of passive voice.'},
-		pointOfView: {name: 'point-of-view', regex: /\w*ly\b/g, title: 'This may be a point of view issue.'},
-		lexicalIllusions: {name: 'lexical-illusions', regex: /\w*ly\b/g, title: 'This may be a lexical illusion.'},
-		misusedWords: {name: 'misused-words', regex: /\w*ly\b/g, title: 'This word is commonly misused.'}
+		fillerWords: {name: 'filler-words', regex: /\bvery\b|\bseveral\b|\bmany\b|\bmost\b|\bfew\b|\bvast\b|\bjust\b|\bquite\b|\boften\b|\bvarious\b|\ba number of\b/g, title: 'This may be an unnecessary filler word.'},
+		passiveVoice: {name: 'passive-voice', regex: /\bwas\b|\bwasn't\b|\bwere\b|\bweren't\b|\bhad\b|\w*ing\b/g, title: 'This may be an unnecessary use of passive voice.'},
+		pronoun: {name: 'pronoun', regex: /\bhe\b|\bshe\b|\bit\b/g, title: 'This may be a point of view issue.'},
+		lexicalIllusions: {name: 'lexical-illusions', regex: /\b(\w+)\s+\1\b/g, title: 'This may be a lexical illusion.'},
+		misusedWords: {name: 'misused-words', regex: /\btheir\b|\bthey're\b|\bthere\b|\byour\b|\byou're\b|\bits\b|\bit's\b|\bwhose\b|\bwho's\b/g, title: 'This word is commonly misused.'}
 	};
 
 })();
