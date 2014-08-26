77e93ebe153926506cac1ef271b680adca8d3575
tylerwalters <tyler@tylerwalters.com>
2014-08-25 20:52:43 -0700

    fixing port

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 app.use(express.static(__dirname, 'dist'));
 app.all(/^\/dist$/, function(req, res) { res.redirect('/dist/'); });
 
 app.use('/',express.static(__dirname+'/dist'));
 
-var server = app.listen(3000, function() {
-	console.log('Listening on port 3000');
+var server = app.listen(port, function() {
+	console.log('Listening on port ' + port);
 });
