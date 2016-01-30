7dc3e17498a4c960f165b2404415a8e7dd8e4455
tylerwalters <tyler@tylerwalters.com>
2016-01-29 23:22:07 -0700

    Switching to gulp over Grunt

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+/* jshint node: true */
+
+'use strict';
+
+var gulp = require('gulp'),
+  jshint = require('gulp-jshint'),
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify');
+
+
+gulp.task('lint', function () {
+  return gulp.src(['src/js/**/*.js', '*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('sass', function () {
+  return gulp.src('src/sass/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sourcemaps.write())
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('js-min', function () {
+  return gulp.src('src/js/script.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(concat('script.min.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('copy', function () {
+  return gulp.src('src/index.html')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('src/sass/**/*.scss', ['sass', 'sass-min']);
+  gulp.watch('src/js/**/*.js', ['lint', 'js-min']);
+  gulp.watch('src/index.html', ['copy']);
+});
+
+gulp.task('minify', ['sass-min', 'js-min']);
+gulp.task('build', ['lint', 'sass', 'js-min', 'copy']);
