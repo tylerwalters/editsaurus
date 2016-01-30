0c0cb0f7aac81c5a88e5bda8f1c21bb2b60d85d9
tylerwalters <tyler@tylerwalters.com>
2016-01-29 23:25:54 -0700

    Cleaning up index file

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var app = express();
-
 var port = process.env.PORT || 8080;
 
-app.use(express.static(__dirname, 'dist'));
-app.all(/^\/dist$/, function(req, res) { res.redirect('/dist/'); });
-
-app.use('/',express.static(__dirname+'/dist'));
+app.use('/',express.static(__dirname + '/dist'));
 
 var server = app.listen(port, function() {
-	console.log('Listening on port ' + port);
+	console.log('Write Check now running.');
+	console.log('Listening on port ' + port + '.');
 });
